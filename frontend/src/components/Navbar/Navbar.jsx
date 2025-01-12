adc03503ed06b6a7934678a27c9bb0d91d39db02
Chathurdha2024 <hasangidahanayake22@gmail.com>
2025-01-12 17:59:11 +0530

    Completed the Navigation bar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 
 const Navbar = () => {
+
+    const [menu,setMenu] = useState("menu");/*Creating a state variable*/
+
   return (
     <div className='navbar'>
       <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        <li>home</li>
-        <li>menu</li>
-        <li>mobile-app</li>
-        <li>contact us</li>
+        <li onClick={()=>setMenu("home")}className={menu=="home"?"active":""}>home</li>{/*if our menu is home this li tag will provide a one classname that is "active".If our menu is not home, it will provide one empty string*/} 
+        <li onClick={()=>setMenu("menu")}className={menu=="menu"?"active":""}>menu</li>{/*when the state is "menu", this second li tag will get the active classname(any of other three will not get the active classname)*/}
+        <li onClick={()=>setMenu("mobile-app")}className={menu=="mobile-app"?"active":""}>mobile-app</li>
+        <li onClick={()=>setMenu("contact us")}className={menu=="contact us"?"active":""}>contact us</li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
