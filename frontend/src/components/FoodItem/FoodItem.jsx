02a4aab57bc331abd9fffeaec2a212bf2d6d48a1
Chathurdha2024 <hasangidahanayake22@gmail.com>
2025-01-14 19:49:51 +0530

    Created the add and remove buttons for ordering food items

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/assets'
 
 const FoodItem = ({id,name,price,description,image}) => {
-  
+
+     const [itemCount,setItemCount] = useState(0)/*The product cart is intialized with zero*/
+
   return (
     <div className='food-item'>
       <div className="food-item-image-container">
         <img className='food-item-image' src={image} alt="" />
+        {!itemCount 
+            ?<img className='add' onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_white} alt="" />
+            :<div className='food-item-counter'>{/*when the clicking count is not equal to zero it's going to another dev and the plus mark dissappears*/}
+                  <img onClick={()=>setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt="" />
+                  <p>{itemCount}</p>
+                  <img onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt="" />
+            </div>
+        }
       </div>
       <div className="food-item-info">
         <div className="food-item-name-rating">
