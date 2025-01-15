ff7212542c2e213ea552e5e2a87586ac3f07721a
Chathurdha2024 <hasangidahanayake22@gmail.com>
2025-01-16 02:33:41 +0530

    Linked the navbar elements to the specific locations in the webpage

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 
@@ -10,10 +11,10 @@ const Navbar = () => {
     <div className='navbar'>
       <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        <li onClick={()=>setMenu("home")}className={menu=="home"?"active":""}>home</li>{/*if our menu is home this li tag will provide a one classname that is "active".If our menu is not home, it will provide one empty string*/} 
-        <li onClick={()=>setMenu("menu")}className={menu=="menu"?"active":""}>menu</li>{/*when the state is "menu", this second li tag will get the active classname(any of other three will not get the active classname)*/}
-        <li onClick={()=>setMenu("mobile-app")}className={menu=="mobile-app"?"active":""}>mobile-app</li>
-        <li onClick={()=>setMenu("contact us")}className={menu=="contact us"?"active":""}>contact us</li>
+        <Link to='/' onClick={()=>setMenu("home")}className={menu=="home"?"active":""}>home</Link>{/*if our menu is home this li tag will provide a one classname that is "active".If our menu is not home, it will provide one empty string*/} 
+        <a href='#explore-menu' onClick={()=>setMenu("menu")}className={menu=="menu"?"active":""}>menu</a>{/*when the state is "menu", this second li tag will get the active classname(any of other three will not get the active classname)*/}
+        <a href='#app-download' onClick={()=>setMenu("mobile-app")}className={menu=="mobile-app"?"active":""}>mobile-app</a>
+        <a href='#footer' onClick={()=>setMenu("contact us")}className={menu=="contact us"?"active":""}>contact us</a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
