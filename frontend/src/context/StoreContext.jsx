c1b4c7fd22a0110774122160fe7053e8c2c732c2
Chathurdha2024 <hasangidahanayake22@gmail.com>
2025-01-18 14:52:49 +0530

    Created functions for the calculation of Subtotal, Total and activated the proceed checkout button to navigate to the order page

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,42 +1,48 @@
 import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
-export const StoreContext = createContext(null)
+export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
 
-    const [cartItems,setCartItems] = useState({});
-
-    const addToCart = (itemId) => {
-        if(!cartItems[itemId]){
-            setCartItems((prev)=>({...prev,[itemId]:1}))
-        }/*if the user is adding items to the cart at very first, this statement is excecuted*/
-        else {
-            setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
-        }
-    } 
-
-    const removeFromCart = (itemId) => {
-        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } /*if the user is adding items to the cart at very first, this statement is excecuted*/ else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     }
+  };
 
-    useEffect(()=>{
-        console.log(cartItems);
-    },[cartItems])
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
 
-    const contextValue = {
-        food_list,
-        cartItems,
-        setCartItems,
-        addToCart,
-        removeFromCart
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) { /*Item is the keyvalue for the cartItems*/
+      if (cartItems[item] > 0) {
+        let itemInfo = food_list.find((product) => product._id === item); /*If productid is matching with the item, it means that product is available in the cart*/
+        totalAmount += itemInfo.price * cartItems[item];
+      }
     }
-    
-    return (
-        <StoreContext.Provider value={contextValue}>
-            {props.children}
-        </StoreContext.Provider>
-    )
-}
+    return totalAmount;
+  };
+
+  const contextValue = {
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
+    getTotalCartAmount
+  };
+
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
+};
 
 export default StoreContextProvider;
